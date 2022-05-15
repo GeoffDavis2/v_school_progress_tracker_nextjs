1f012305776ebe26bdc4ad319bd6547153f793dc
Geoff Davis <Davis.Geoff2@gmail.com>
2022-05-15 16:51:16 -0700

    Added content to progress-data

--- pages/progress-data.js
+++ pages/progress-data.js
@@ -1,3 +1,16 @@
+import { useTheContext } from '../hocs/context-provider';
+
 export default function Home() {
-  return <div>hello from progress data</div>;
+  const { studentProgress } = useTheContext();
+  console.log(studentProgress[1]?.dt);
+  return (
+    <>
+      <div>{studentProgress.length}</div>
+      {studentProgress?.map((obj) => (
+        <div key={obj.dt}>
+          {obj.dt}, {obj.pts}
+        </div>
+      ))}
+    </>
+  );
 }
