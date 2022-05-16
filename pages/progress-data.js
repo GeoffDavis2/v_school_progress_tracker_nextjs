816f56fdd0826c653c6795eade14fc0546617fcc
Geoff Davis <Davis.Geoff2@gmail.com>
2022-05-15 17:09:02 -0700

    just all students and selectedstudent

--- pages/progress-data.js
+++ pages/progress-data.js
@@ -1,16 +1,21 @@
 import { useTheContext } from '../hocs/context-provider';
 
 export default function Home() {
-  const { studentProgress } = useTheContext();
+  const { studentProgress, allStudents, selectedStudent } = useTheContext();
+  console.log(selectedStudent);
+  console.log(allStudents[0]);
   console.log(studentProgress[1]?.dt);
   return (
     <>
-      <div>{studentProgress.length}</div>
+      {/* <div>{studentProgress.length}</div>
       {studentProgress?.map((obj) => (
         <div key={obj.dt}>
           {obj.dt}, {obj.pts}
         </div>
-      ))}
+      ))} */}
+      {allStudents[1]?.studentName}
+      <br />
+      {selectedStudent?.studentName}
     </>
   );
 }
