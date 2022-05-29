81076738af3cbd74fd8f596ddc0478cbe31f354f
Geoff Davis <Davis.Geoff2@gmail.com>
2022-05-29 12:25:08 -0700

    Working on Student-Info component

--- components/student-dashboard/Student-Info.js
+++ components/student-dashboard/Student-Info.js
@@ -1,10 +1,14 @@
+import { useTheContext } from '../../hocs/context-provider';
+
 export const StudentInfo = () => {
+  const { selectedStudent } = useTheContext();
+
   return (
     <div
       className="student_dashboard_component"
       style={{ gridArea: '1 / 1 / 2 / 3' }}
     >
-      Student Info
+      {selectedStudent.studentName}
     </div>
   );
 };
